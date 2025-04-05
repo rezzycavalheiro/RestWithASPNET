0a5c6f8d436a61056b362c1b4e9d1376f18522ea
Renata Cavalheiro da Silva <renatacavalheiro15@gmail.com>
2025-04-05 00:33:20 -0300

    Changing to person

--- RestWithASPNET/Controllers/PersonController.cs
+++ RestWithASPNET/Controllers/PersonController.cs
@@ -4,11 +4,11 @@ namespace RestWithASPNET.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CalculatorController : ControllerBase
+    public class PersonController : ControllerBase
     {
-        private readonly ILogger<CalculatorController> _logger;
+        private readonly ILogger<PersonController> _logger;
 
-        public CalculatorController(ILogger<CalculatorController> logger)
+        public PersonController(ILogger<PersonController> logger)
         {
             _logger = logger;
         }
@@ -16,92 +16,9 @@ namespace RestWithASPNET.Controllers
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
-            {
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
-            }
+
         
             return BadRequest("Invalid input.");     
-        }
-
-        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
-        public IActionResult Subtraction(string firstNumber, string secondNumber)
-        {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
-            {
-                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
-                return Ok(subtraction.ToString());
-            }
-
-            return BadRequest("Invalid input.");
-        }
-
-
-        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
-        public IActionResult Multiplication(string firstNumber, string secondNumber)
-        {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
-            {
-                var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
-                return Ok(multiplication.ToString());
-            }
-
-            return BadRequest("Invalid input.");
-        }
-
-        [HttpGet("mean/{firstNumber}/{secondNumber}")]
-        public IActionResult Mean(string firstNumber, string secondNumber)
-        {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
-            {
-                var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
-                return Ok(mean.ToString());
-            }
-
-            return BadRequest("Invalid input.");
-        }
-
-        [HttpGet("division/{firstNumber}/{secondNumber}")]
-        public IActionResult Division(string firstNumber, string secondNumber)
-        {
-            if (ConvertToDecimal(secondNumber) > 0 && (IsNumeric(firstNumber) && IsNumeric(secondNumber)))
-            {
-                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
-            }
-
-            return BadRequest("Invalid input.");
-        }
-
-        [HttpGet("sqrt/{firstNumber}/{secondNumber}")]
-        public IActionResult Sqrt(string firstNumber)
-        {
-            if (IsNumeric(firstNumber))
-            {
-                var sqrt = Math.Sqrt((double)ConvertToDecimal(firstNumber));
-                return Ok(sqrt.ToString());
-            }
-
-            return BadRequest("Invalid input.");
-        }
-        private decimal ConvertToDecimal(string strNumber)
-        {
-            decimal decimalValue;
-            if (decimal.TryParse(strNumber, out decimalValue))
-            {
-                return decimalValue;
-            }
-
-            return 0;
-        }
-
-        private bool IsNumeric(string strNumber)
-        {
-            double number;
-            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
-
-            return isNumber;
         }
     }
 }
---
+++ RestWithASPNET/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNET.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
